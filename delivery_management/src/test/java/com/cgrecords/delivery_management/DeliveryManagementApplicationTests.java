43fd5880db4eab1bd6239c5c8e0cfa66f88fdc3a
Adam Hawke <ah14275@my.bristol.ac.uk>
2022-07-24 13:57:29 +0100

    Initial commit

---
+++ delivery_management/src/test/java/com/cgrecords/delivery_management/DeliveryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgrecords.delivery_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
